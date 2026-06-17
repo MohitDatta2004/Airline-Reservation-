791887a86e722daeee41e07183e54bfd72c5ff45
MohitDatta2004 <mkdatta349@gmail.com>
2026-06-17 16:18:53 +0530

    Add files via upload

---
+++ manage_airline.py
@@ -0,0 +1,129 @@
+from tkinter import *
+from tkinter.ttk import Treeview
+from tkinter import messagebox
+from . import database, add_airlines
+from tkinter import ttk
+from PIL import Image, ImageTk
+
+
+class ManageAirline:
+    def __init__(self):
+        self.t = Toplevel()
+        self.t.title("Manage Airlines | Airline Reservation | Admin Panel")
+        self.fullwidth = self.t.winfo_screenwidth()
+        self.fullheight = self.t.winfo_screenheight()
+        self.width = int((self.fullwidth - 800) / 2)
+        self.height = int((self.fullheight - 500) / 2) - 40
+        s = "800x500+" + str(self.width) + "+" + str(self.height)
+        self.t.geometry(s)
+        self.icon = PhotoImage(file="admin/images/icn2.png")
+        self.t.iconphoto(False, self.icon)
+
+    def widgets(self):
+        self.image_path = Image.open('admin/images/img8.jpg')
+        self.image = self.image_path.resize((800, 500))
+        self.background_image = ImageTk.PhotoImage(self.image)
+        print("Image path:- ", self.image_path)
+
+        self.i = Canvas(self.t, width='800', height='500')
+        self.i.place(x='0', y='0')
+        self.i.create_image('0', '0', image=self.background_image, anchor=NW)
+
+        self.i.create_text(430, 50, text='AIRCRAFT DETAILS', fill='Black',
+                           font=("Century Gothic bold", 20))
+
+        self.add_aircraft_button = Button(self.t, text="Add Airline", bg='Black', fg='White', width='20',
+                                          font=("Century Gothic bold", 10), command=self.open_add_airline_page)
+        self.add_aircraft_button.place(x=650, y=35, width="100", height="30")
+
+        self.f = Frame(self.t, width='700', height='350')
+        self.f.place(x=50, y=100)
+
+        style = ttk.Style()
+        style.configure("mystyle.Treeview", highlightthickness=0, bd=0, font=('Century Gothic', 10))
+        style.configure("mystyle.Treeview.Heading", font=('Century Gothic bold', 10, 'bold'))
+
+        self.tr = Treeview(self.f, columns=("A", "B", "C", "D", "E", "F", "G", "H"), selectmode='extended',
+                           style="mystyle.Treeview")
+
+        self.tr.heading("#0", text="ID")
+        self.tr.column("#0", minwidth=100, width=100, stretch=True, anchor=CENTER)
+
+        self.tr.heading("#1", text="AIRLINE ID")
+        self.tr.column("#1", minwidth=100, width=100, stretch=True, anchor=CENTER)
+
+        self.tr.heading("#2", text="AIRLINE NAME")
+        self.tr.column("#2", minwidth=100, width=100, stretch=True, anchor=CENTER)
+
+        self.tr.heading("#3", text="FOUNDED YEAR")
+        self.tr.column("#3", minwidth=100, width=100, stretch=True, anchor=CENTER)
+
+        self.tr.heading("#4", text="COUNTRY")
+        self.tr.column("#4", minwidth=100, width=100, stretch=True, anchor=CENTER)
+
+        self.tr.heading("#5", text="HEADQUARTERS LOCATION")
+        self.tr.column("#5", minwidth=100, width=100, stretch=True, anchor=CENTER)
+
+        self.tr.heading("#6", text="DELETE")
+        self.tr.column("#6", minwidth=100, width=100, stretch=True, anchor=CENTER)
+
+        self.tr.heading("#7", text="UPDATE")
+        self.tr.column("#7", minwidth=100, width=100, stretch=True, anchor=CENTER)
+
+        data = database.get_airlines()
+        for i in data:
+            self.tr.insert("", 0, text=i[0], values=(i[1], i[2], i[3], i[4], i[5], "Delete", "Update"))
+
+        # Constructing vertical scroll bar on treeview
+        self.vertical_scrollbar = Scrollbar(self.tr, orient="vertical", command=self.tr.yview)
+        self.vertical_scrollbar.pack(side='right', fill='y')
+        self.tr.configure(yscrollcommand=self.vertical_scrollbar.set)
+
+        # Constructing horizontal scroll bar on treeview
+        self.horizontal_scrollbar = Scrollbar(self.tr, orient="horizontal", command=self.tr.xview)
+        self.horizontal_scrollbar.pack(side='bottom', fill='x')
+        self.tr.configure(xscrollcommand=self.horizontal_scrollbar.set)
+
+        self.tr.bind("<Double Button-1>", self.actions)
+        self.tr.place(x=1, y=1, width="700", height="350")
+
+        self.t.mainloop()
+
+    def actions(self, e):
+        row = self.tr.focus()
+        print("Focused row:", row)
+
+        column = self.tr.identify_column(e.x)
+        print("column"), column
+
+        data = self.tr.item(row)
+        print("Data of focused row", data)
+
+        r_id = (data.get("text"),)
+        print("id of focused row", r_id)
+
+        if column == "#6":
+            choice = messagebox.askyesno("Alert", "Do you really want to delete these airline details?")
+            if choice:
+                result = database.delete_data(r_id)
+                if result:
+                    messagebox.showinfo("Message", "Airline details deleted successfully")
+                    self.t.destroy()
+                    a = ManageAirline()
+                    a.widgets()
+                else:
+                    messagebox.showwarning("Alert", "Something went wrong")
+        elif column == "#7":
+            a = add_airlines.AddAirlines(self.tr.item(row))
+            self.t.destroy()
+            a.widgets()
+
+    def open_add_airline_page(self):
+        self.t.destroy()
+        g = add_airlines.AddAirlines()
+        g.widgets()
+
+
+if __name__ == "__main__":
+    a = ManageAirline()
+    a.widgets()
